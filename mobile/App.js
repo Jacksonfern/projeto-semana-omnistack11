572080fead06f72f2ad9e3f5433290bb79b10ea7
Jackson <jackson.fernandes@icomp.ufam.edu.br>
2020-03-27 17:23:42 -0400

    Aplicação mobile completa!

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+import React from 'react';
+
+import Routes from './src/routes';
+
+export default function App() {
+  return (
+    <Routes/>
+  );
+}
+
+/*Cada componente deve ter estilização própria
+ *Não existe herança de estilo*/
